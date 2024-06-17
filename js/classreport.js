a6d3a7b0af2e12cd13847c7cd9099cc0743785b8
norona-marynicole <marynicolenorona4@gmail.com>
2024-06-17 22:31:46 +0800

    add new changes

--- js/classreport.js
+++ js/classreport.js
@@ -1,34 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document
-      .getElementById("attendanceForm")
-      .addEventListener("submit", function (event) {
-        event.preventDefault();
-  
-        const courses = document.getElementById("courses").value;
-        const students = document.getElementById("students").value;
-        const calendar = document.getElementById("calendar").value;
-        const status = document.querySelector(
-          'input[name="status"]:checked'
-        ).value;
-        const classReport = document.getElementById("class-report").value;
-  
-        if (!courses || !students || !calendar || !status || !classReport) {
-          alert("Please fill out all fields.");
-          return;
-        }
-  
-        const formData = {
-          courses: courses,
-          students: students,
-          calendar: calendar,
-          status: status,
-          classReport: classReport,
-        };
-  
-        console.log("Form Data:", formData);
-        alert("Form submitted successfully!");
-  
-        document.getElementById("attendanceForm").reset();
-      });
+  const form = document.getElementById("attendanceForm");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const courses = document.getElementById("courses").value;
+    const students = document.getElementById("students").value;
+    const calendar = document.getElementById("calendar").value;
+    const present = document.getElementById("presentCount").textContent;
+    const absent = document.getElementById("absentCount").textContent;
+    const classReport = document.getElementById("class-report").value;
+
+    if (
+      !courses ||
+      !students ||
+      !calendar ||
+      !present ||
+      !absent ||
+      !classReport
+    ) {
+      alert("Please fill out all fields.");
+      return;
+    }
+
+    const formData = {
+      courses: courses,
+      students: students,
+      calendar: calendar,
+      present: present,
+      absent: absent,
+      classReport: classReport,
+    };
+
+    console.log("Form Data:", formData);
+
+    alert("Form submitted successfully!");
+
+    form.reset();
   });
-  
+});
