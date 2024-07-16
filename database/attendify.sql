ad8f936f0073cb8e920c6b330c041b63731826cd
Paul Angelo Reyes <paulangeloreyes.official@gmail.com>
2024-07-16 23:30:01 +0800

    update database

--- database/attendify.sql
+++ database/attendify.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Jun 08, 2024 at 06:50 AM
--- Server version: 8.0.31
--- PHP Version: 8.0.26
+-- Host: 127.0.0.1
+-- Generation Time: Jul 16, 2024 at 05:27 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,16 +27,14 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
-DROP TABLE IF EXISTS `admin`;
-CREATE TABLE IF NOT EXISTS `admin` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `admin_id` varchar(100) NOT NULL,
-  `datetime_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `datetime_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `datetime_created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `datetime_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -44,18 +42,16 @@ CREATE TABLE IF NOT EXISTS `admin` (
 -- Table structure for table `attendance`
 --
 
-DROP TABLE IF EXISTS `attendance`;
-CREATE TABLE IF NOT EXISTS `attendance` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `class_no` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+CREATE TABLE `attendance` (
+  `id` int(11) NOT NULL,
+  `class_no` varchar(100) NOT NULL,
   `date` varchar(100) NOT NULL,
   `status` varchar(100) NOT NULL,
   `attendance_id` varchar(100) NOT NULL,
   `student_no` varchar(100) NOT NULL,
-  `datetime_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `datetime_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `datetime_created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `datetime_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `attendance`
@@ -71,16 +67,15 @@ INSERT INTO `attendance` (`id`, `class_no`, `date`, `status`, `attendance_id`, `
 -- Table structure for table `class`
 --
 
-DROP TABLE IF EXISTS `class`;
-CREATE TABLE IF NOT EXISTS `class` (
-  `id` int NOT NULL,
+CREATE TABLE `class` (
+  `id` int(11) NOT NULL,
   `class_no` varchar(100) NOT NULL,
   `student_no` varchar(100) NOT NULL,
   `courses_id` varchar(100) NOT NULL,
   `prof_id` varchar(100) NOT NULL,
-  `datetime_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `datetime_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `datetime_created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `datetime_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -88,16 +83,14 @@ CREATE TABLE IF NOT EXISTS `class` (
 -- Table structure for table `courses`
 --
 
-DROP TABLE IF EXISTS `courses`;
-CREATE TABLE IF NOT EXISTS `courses` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `courses` (
+  `id` int(11) NOT NULL,
   `courses_id` varchar(100) NOT NULL,
   `course_def` varchar(100) NOT NULL,
   `course_code` varchar(100) NOT NULL,
-  `datetime_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `datetime_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `datetime_created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `datetime_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `courses`
@@ -112,26 +105,24 @@ INSERT INTO `courses` (`id`, `courses_id`, `course_def`, `course_code`, `datetim
 -- Table structure for table `professor`
 --
 
-DROP TABLE IF EXISTS `professor`;
-CREATE TABLE IF NOT EXISTS `professor` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `middlename` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `lastname` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+CREATE TABLE `professor` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `middlename` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
   `birthday` varchar(100) NOT NULL,
-  `age` int NOT NULL,
+  `age` int(11) NOT NULL,
   `gender` varchar(100) NOT NULL,
   `address` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `phone` bigint NOT NULL,
+  `phone` bigint(20) NOT NULL,
   `course` varchar(100) NOT NULL,
   `status` varchar(100) NOT NULL,
-  `password` int NOT NULL,
+  `password` int(11) NOT NULL,
   `prof_id` varchar(100) NOT NULL,
-  `datetime_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `datetime_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `datetime_created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `datetime_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `professor`
@@ -146,24 +137,22 @@ INSERT INTO `professor` (`id`, `firstname`, `middlename`, `lastname`, `birthday`
 -- Table structure for table `student`
 --
 
-DROP TABLE IF EXISTS `student`;
-CREATE TABLE IF NOT EXISTS `student` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `middlename` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `lastname` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `middlename` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
   `birthday` varchar(100) NOT NULL,
-  `age` int NOT NULL,
+  `age` int(11) NOT NULL,
   `gender` varchar(100) NOT NULL,
   `address` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `phone` bigint NOT NULL,
+  `phone` bigint(20) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `datetime_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `datetime_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `student_number` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `datetime_created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `datetime_updated` timestamp NOT NULL DEFAULT current_timestamp(),
+  `student_number` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `student`
@@ -172,6 +161,74 @@ CREATE TABLE IF NOT EXISTS `student` (
 INSERT INTO `student` (`id`, `firstname`, `middlename`, `lastname`, `birthday`, `age`, `gender`, `address`, `email`, `phone`, `password`, `datetime_created`, `datetime_updated`, `student_number`) VALUES
 (1, 'John Ruiz', 'P', 'Reyes', '', 23, 'Male', 'Central Signal', 'johnruizreyes@gmail.com', 9123456789, 'akoaysibabyboy', '2024-06-08 05:13:22', '2024-06-08 05:13:22', '2022-00001-TG-0'),
 (2, 'Paul Angelo', ' ', 'Reyes', '', 20, 'Male', 'Lower Bicutan', 'paulangeloreyes@gmail.com', 9123456789, 'pagodnasabuhay', '2024-06-08 05:13:22', '2024-06-08 05:13:22', '2022-00002-TG-0');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `professor`
+--
+ALTER TABLE `professor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `professor`
+--
+ALTER TABLE `professor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
